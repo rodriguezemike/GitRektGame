6254dff41bc67cdf244377d3513057b089c5ee98
Michelle Wong <michellewong.nma@gmail.com>
2015-10-15 16:33:29 -0700

    added combat ui

---
+++ GitRekt/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ChangeToScene(int sceneToChangeTo){
+		//give index to each scene and call them by their index(int)
+		//note: can check index from build menu and change index of scenes
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
---
+++ GitRekt/Assets/Scripts/Spells.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spells : MonoBehaviour {
+
+	public void CallSpell(int spell_num)
+	{
+		if(spell_num == 1)
+			print ("spell 1");
+		if(spell_num == 2)
+			print ("spell 2");
+		if(spell_num == 3)
+			print ("spell 3");
+		if(spell_num == 4)
+			print ("spell 4");
+	}
+
+	public void BasicAttack()
+	{
+		print ("attack!!");
+	}
+}
